cd5601d12eb0ab64057b666ff96a4f0faff4f590
Franciszek Czana <franciszek.czana@dazn.com>
2024-12-22 13:29:26 +0100

    solve task 6

--- AOC2023/Task/2024/Task6Y2024.swift
+++ AOC2023/Task/2024/Task6Y2024.swift
@@ -7,7 +7,6 @@ final class Task6Y2024: TaskProvider {
 
     init(data: [String]) {
         self.data = data
-
     }
 
     struct SearchPoint: Hashable {
@@ -17,19 +16,60 @@ final class Task6Y2024: TaskProvider {
 
     func solveA() -> Int {
 
-        var searchPoint: SearchPoint?
-        for (lineIndex, line) in taskData.enumerated() {
-            if let char = line.enumerated().first(where: { $1.contains("^") }) {
-                searchPoint = .init(x: Int( char.offset), y: Int(lineIndex))
-                break
+        let searchPoints = findAllSearchPoints()
+
+        return searchPoints.count
+    }
+
+    func solveB() -> Int {
+
+        var searchPoints = findAllSearchPoints()
+        searchPoints.removeFirst()
+
+        guard let startOriginSearchPoint = findStartSearchPoint() else { return 0 }
+
+        var result = 0
+        for searchPoint in searchPoints {
+            resetDirection()
+
+            var taskData2 = taskData
+            taskData2[searchPoint.y][searchPoint.x] = "#"
+            
+            var startSearchPoint = startOriginSearchPoint
+            var history: [SearchPoint: [Direction]] = [:]
+            
+            while let point = go(from: startSearchPoint, using: taskData2) {
+                if taskData2[safe: point.y]?[safe: point.x] == "#" {
+                    rotateRight()
+                } else {
+                    if history[point]?.contains(direction) == true {
+                        result += 1
+                        break
+                    } else {
+                        if history[point] == nil {
+                            history[point] = [direction]
+                        } else {
+                            history[point]?.append(direction)
+                        }
+                      
+                        startSearchPoint = point
+                    }
+                }
             }
+            
         }
 
-        guard var searchPoint else { return 0 }
+        return result
+    }
+    
+    func findAllSearchPoints() -> Set<SearchPoint> {
+        resetDirection()
+
+        guard var searchPoint = findStartSearchPoint() else { return [] }
 
         var searchedPoints: Set<SearchPoint> = [searchPoint]
 
-        while let point = go(from: searchPoint) {
+        while let point = go(from: searchPoint, using: taskData) {
             if taskData[safe: point.y]?[safe: point.x] == "#" {
                 rotateRight()
             } else {
@@ -38,12 +78,7 @@ final class Task6Y2024: TaskProvider {
             }
         }
         
-        return searchedPoints.count
-    }
-
-    func solveB() -> Int {
-        
-        return 0
+        return searchedPoints
     }
     
     private var direction: Direction = .up
@@ -55,7 +90,7 @@ private extension Task6Y2024 {
         case up, down, left, right
     }
 
-    func go(from point: SearchPoint) -> SearchPoint? {
+    func go(from point: SearchPoint, using taskData: [[String]]) -> SearchPoint? {
         switch direction {
         case .up:
             taskData[safe: point.y-1]?[safe: point.x] != nil ? SearchPoint(x: point.x, y: point.y-1) : nil
@@ -75,5 +110,19 @@ private extension Task6Y2024 {
         case .up: .right
         case .right: .down
         }
+    }
+
+    func resetDirection() {
+        direction = .up
+    }
+
+    func findStartSearchPoint() -> SearchPoint? {
+        for (lineIndex, line) in taskData.enumerated() {
+            if let char = line.enumerated().first(where: { $1.contains("^") }) {
+                return .init(x: Int( char.offset), y: Int(lineIndex))
+            }
+        }
+
+        return nil
     }
 }
